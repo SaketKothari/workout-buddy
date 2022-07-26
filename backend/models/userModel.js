a87e05bfbbb61a13ffe13c2a80814822eba31fb2
Saket Kothari <saketkothari722@gmail.com>
2022-07-26 15:56:30 +0530

    Logging Users In

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,7 +30,7 @@ userSchema.statics.signup = async function (email, password) {
   if (!validator.isStrongPassword(password)) {
     throw Error('Password not strong enough');
   }
- 
+
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -41,6 +41,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// Static Login Method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error('Incorrect Email');
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error('Incorrect Password');
+  }
 
   return user;
 };
